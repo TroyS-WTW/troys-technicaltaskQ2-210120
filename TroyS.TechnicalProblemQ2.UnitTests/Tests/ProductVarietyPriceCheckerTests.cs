1f43b48ccb78e58cb961bf33cf4cd05c9fe09622
Troy Sijuwade <troy.sijuwade@yahoo.co.uk>
2020-01-21 13:16:48 +0000

    Implemented a failing happy path test to check Product Variety price conversions in EURO are correct

---
+++ TroyS.TechnicalProblemQ2.UnitTests/Tests/ProductVarietyPriceCheckerTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace TroyS.TechnicalProblemQ2.UnitTests.Tests
+{
+    [TestFixture]
+    public class ProductVarietyPriceCheckerTests
+    {
+        [TestCase(10, 15)]
+        [TestCase(20, 30)]
+        [TestCase(22, 33)]
+        [TestCase(28, 42)]
+        [TestCase(12, 18)]
+        [TestCase(15, 22.5)]
+        [TestCase(60, 90)]
+        [TestCase(14, 21)]
+        [TestCase(24, 36)]
+        [TestCase(45, 67.5)]
+        public void Product_Variety_Price_In_Euro_Is_Correct(decimal priceInGbp, decimal expectedPriceInEuro)
+        {
+            throw new NotImplementedException();
+        }
+
+
+
+    }
+}
